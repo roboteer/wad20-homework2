85b25def29001cbd6e12b682c929c6c23712ac57
Ott Oopkaup <alk@ut.ee>
2020-10-25 18:55:40 +0200

    task3 follow button toggle

--- res/js/browse.js
+++ res/js/browse.js
@@ -10,8 +10,9 @@ $(function() {
     /* Like button toggle*/
     /* Like button is loaded dynamically, so we attach function to static class 'main-container'*/
     $('.main-container').on('click',".follow-button",function () {    
-        var postId = $(this).data('postid');    
-        $('.like-button[data-postid="'+postId+'"]').toggleClass('clicked');
+        var avatarId = $(this).data('avatarid');
+        console.log("press on " + $(this).data('avatarid')) 
+        $('.follow-button[data-avatarid="'+avatarId+'"]').toggleClass('followed');
     });
     
     /* Fetch user info and put it into dropdown */
@@ -33,7 +34,7 @@ let profileTemplate = '<div class="profile-browse">\n' +
 '               <img src="res/images/avatar.png" class="avatar-browse" alt="Me">\n' +
 '       </div>\n' +
 '       <div class="profile-name"><h3>Gordon Freeman</h3></div>\n' +
-'               <div><button>Follow</button></div>\n' +
+'       <div class="profile-actions"><button type="button" name="follow" class="follow-button">Follow</button></div>\n' +
 '       </div>\n' +
 '</div>'
 
@@ -46,6 +47,8 @@ function updateAvatarInfo(avatars) {
         
         $('.avatar-container img', profileElement).attr('src', avatar.avatar);
         $('.profile-name h3', profileElement).text(avatar.firstname + " " + avatar.lastname);
+        $('.follow-button', profileElement).attr('data-avatarid', avatar.firstname );
+
         $('section.main-container').append(profileElement);
     }
 }
