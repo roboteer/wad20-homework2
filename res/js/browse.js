6060873a22dc25d93395f48a6b166aef310e9388
Ott Oopkaup <alk@ut.ee>
2020-10-25 18:35:17 +0200

    task 3 profile pics, some scrambles of other

--- res/js/browse.js
+++ res/js/browse.js
@@ -9,26 +9,48 @@ $(function() {
 
     /* Like button toggle*/
     /* Like button is loaded dynamically, so we attach function to static class 'main-container'*/
-    $('.main-container').on('click',".like-button",function () {    
+    $('.main-container').on('click',".follow-button",function () {    
         var postId = $(this).data('postid');    
-        $('.like-button[data-postid="'+postId+'"]').toggleClass('liked');
+        $('.like-button[data-postid="'+postId+'"]').toggleClass('clicked');
     });
     
     /* Fetch user info and put it into dropdown */
     $.get('https://private-anon-6783f8eed5-wad20postit.apiary-mock.com/users/1')
         .done( (data) => updateUserInfo(data) )
         .fail((error) => console.log(error));
-
+    /* same for profiles  */
+    $.get('https://private-anon-f4948f1c5a-wad20postit.apiary-mock.com/profiles')
+        .done( (data) => updateAvatarInfo(data) )
+        .fail((error) => console.log(error));
 
     
 
 
 })
 
+let profileTemplate = '<div class="profile-browse">\n' +
+'       <div class="avatar-container">\n' +
+'               <img src="res/images/avatar.png" class="avatar-browse" alt="Me">\n' +
+'       </div>\n' +
+'       <div><h3>Gordon Freeman</h3></div>\n' +
+'               <div><button>Follow</button></div>\n' +
+'       </div>\n' +
+'</div>'
 
+
+function updateAvatarInfo(avatars) {
+    
+    for (avatar of avatars){
+        console.log(avatar)
+        let profileElement = $(profileTemplate);
+        
+        $('.avatar-container img', profileElement).attr('src', avatar.avatar);
+        $('.profile-browse h3', profileElement).text(avatar.firstname + " " + avatar.lastname);
+        $('section.main-container').append(profileElement);
+    }
+}
 
 function updateUserInfo(data) {
-    console.log(data);
 
     let userinfo = $('<span>').text(data.firstname + " " + data.lastname);
     let email = $('<span>').text( data.email );
